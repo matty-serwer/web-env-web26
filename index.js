80fa7f12fa9cd76d6db7158999056ec6f75c5c59
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-12-03 12:55:24 -0500

    yo

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const port = process.env.PORT || 4000
+
+console.log('web whatever');
+console.log(__dirname);
+console.log(process.env.LADY);
+console.log(process.env.FOO)
+console.log(process.env.ALWAYS)
+console.log(process.env.PORT)
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// api
+app.use('/api/*', (_, res) => {
+    res.json({ data: 'D000000d'})
+})
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+})
