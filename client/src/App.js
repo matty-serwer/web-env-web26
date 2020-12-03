ded011cfe563954847f35d6d0c8afc898f5f8941
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-12-03 13:54:50 -0500

    fixed path in react app

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { useState, useEffect } from 'react';
 function url(path) {
   return process.env.NODE_ENV === 'development'
    ? `http://localhost:5000${path}`
-   : ''
+   : path
 }
 
 function App() {
