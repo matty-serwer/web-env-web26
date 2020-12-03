1a07c4ed16b71117343357d76339a5dc7473ae51
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-12-03 13:36:19 -0500

    outside client folder

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const express = require('express');
 const port = process.env.PORT || 4000
 
+const path = require('path');
+
 console.log('web whatever');
 console.log(__dirname);
 console.log(process.env.LADY);
@@ -14,10 +16,16 @@ console.log(process.env.PORT)
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // api
 app.use('/api/*', (_, res) => {
     res.json({ data: 'D000000d'})
+})
+
+app.use('*', (_, res) => {
+    //send back index.html
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 })
 
 app.listen(port, () => {
