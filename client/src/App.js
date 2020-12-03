ebe14ec5375690ba70ac9caeb894f24406669096
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-12-03 13:34:45 -0500

    built react app, and made connections

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Web 36 Yo
+      </header>
+    </div>
+  );
+}
+
+export default App;
